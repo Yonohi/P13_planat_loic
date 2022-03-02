a4bc6084fcac88eae621db7533667f9497bef74e
Yonohi <loic.planat2@gmail.com>
2022-03-02 13:04:46 +0100

    deplacement views et urls pour profiles

--- lettings/urls.py
+++ lettings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from . import views
+
 
 urlpatterns = [
     path('', views.lettings_index, name='lettings_index'),
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from lettings import urls as lettings_urls
+from profiles import urls as profiles_urls
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include(lettings_urls)),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', include(profiles_urls)),
     path('admin/', admin.site.urls),
 ]
