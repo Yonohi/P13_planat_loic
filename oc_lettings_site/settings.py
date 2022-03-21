792e46a70a424a3a7c124786386c94f3ec68a187
Yonohi <loic.planat2@gmail.com>
2022-03-21 16:52:34 +0100

    test static root(3)

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -107,6 +107,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# Help for static
 # https://devcenter.heroku.com/articles/django-assets
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
