1352b57599361edd59b6a3271944b35679264bcf
Yonohi <loic.planat2@gmail.com>
2022-03-22 14:06:53 +0100

    test admin css debug false(3)

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 from lettings import urls as lettings_urls
 from profiles import urls as profiles_urls
 from . import views
@@ -10,4 +13,6 @@ urlpatterns = [
     path('lettings/', include(lettings_urls)),
     path('profiles/', include(profiles_urls)),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
