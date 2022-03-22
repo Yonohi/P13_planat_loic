ebb547af1ceec110d83ec7b983df4890e9362fd7
Yonohi <loic.planat2@gmail.com>
2022-03-22 14:02:27 +0100

    test admin css debug false(2)

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -113,10 +113,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # Help for static
 # https://devcenter.heroku.com/articles/django-assets
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
